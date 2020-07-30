22cdd52f2c1e301c442421ce19222938e2c6ad83
Sebastian Rangel <sebastianrangel@iMac-de-Sebastian.local>
2020-07-30 17:01:11 +0200

    create new script

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -7,6 +7,7 @@ public class Weapons : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
+    [SerializeField] float damage = 30f;
     void Update()
     {
         if (Input.GetButtonDown("Fire1")) 
@@ -18,7 +19,16 @@ public class Weapons : MonoBehaviour
     private void Shoot()
     {
         RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-        Debug.Log("I hit this thing :" + hit.transform.name);
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("I hit this thing :" + hit.transform.name);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else 
+        {
+            return;
+        }
     }
 }
