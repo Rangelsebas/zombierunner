d7d3f809fc5e140d966df94df452e530d48a4c71
Sebastian Rangel <sebastianrangel@iMac-de-Sebastian.local>
2020-08-02 23:08:03 +0200

    add weapon zoom script

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    public Camera fpsCamera;
+
+    public float zoomedOutFOV = 90f;
+    public float zoomedInFOV = 30;
+
+
+    bool zoomedInToggle = false;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (zoomedInToggle == false)
+            {
+                zoomedInToggle = true;
+                fpsCamera.fieldOfView = zoomedInFOV;
+            }
+            else 
+            {
+                zoomedInToggle = false;
+                fpsCamera.fieldOfView = zoomedOutFOV;
+            }
+        }
+    }
+}
